b12b0f228d57f0faad2e5d03c395460d983165d6
aakashmangla2000 <akkibaby90@yahoo.com>
2021-12-21 14:10:08 +0530

    Changed config.exs for local server

--- task2/task_2_remote_communication/task_2_phoenix_client/config/config.exs
+++ task2/task_2_remote_communication/task_2_phoenix_client/config/config.exs
@@ -21,8 +21,8 @@ use Mix.Config
 #     config :logger, level: :info
 #
 
-config :phoenix_server, url: "ws://64c1-117-97-180-140.ngrok.io:80/socket/websocket"
-# config :phoenix_server, url: "ws://localhost:4000/socket/websocket"
+# config :phoenix_server, url: "ws://64c1-117-97-180-140.ngrok.io:80/socket/websocket"
+config :phoenix_server, url: "ws://localhost:4000/socket/websocket"
 
 # It is also possible to import configuration files, relative to this
 # directory. For example, you can emulate configuration per environment
