ee1b65839eebb4600ac0577d2e0f96321f928899
aakashmangla2000 <akkibaby90@yahoo.com>
2021-12-29 20:19:40 +0530

    Deps added for both a and b

--- task4/task4A/line_follower/mix.exs
+++ task4/task4A/line_follower/mix.exs
@@ -21,6 +21,7 @@ defmodule LineFollower.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:circuits_gpio, "~> 0.4"}, {:pigpiox, "~> 0.1.2"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
