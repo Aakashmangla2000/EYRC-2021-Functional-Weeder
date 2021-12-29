99f6bd4fc7e3ae75fee6b60f3291614aea1100c9
aakashmangla2000 <akkibaby90@yahoo.com>
2021-12-29 20:35:14 +0530

    Made line follower working

--- task4/task4A/line_follower/mix.exs
+++ task4/task4A/line_follower/mix.exs
@@ -5,7 +5,7 @@ defmodule LineFollower.MixProject do
     [
       app: :line_follower,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
