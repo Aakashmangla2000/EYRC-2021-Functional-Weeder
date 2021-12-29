9400c9479b50f623182b80575ed043acfe519aa7
aakashmangla2000 <akkibaby90@yahoo.com>
2021-12-29 19:19:37 +0530

    Task 4 added

---
+++ task4/task4A/line_follower/lib/line_follower.ex
@@ -0,0 +1,18 @@
+defmodule LineFollower do
+  @moduledoc """
+  Documentation for `LineFollower`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LineFollower.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
