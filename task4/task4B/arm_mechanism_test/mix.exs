9ea417006718ba2399947798268cb434818d45aa
drishti-agarwal <drishtis.agarwal@gmail.com>
2022-01-12 14:02:14 +0000

    weeding mechanism

--- task4/task4B/arm_mechanism_test/mix.exs
+++ task4/task4B/arm_mechanism_test/mix.exs
@@ -5,7 +5,7 @@ defmodule ArmMechanismTest.MixProject do
     [
       app: :arm_mechanism_test,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
