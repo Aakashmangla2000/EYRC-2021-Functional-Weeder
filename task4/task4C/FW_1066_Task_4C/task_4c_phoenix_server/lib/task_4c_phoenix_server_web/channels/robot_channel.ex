33b67bda0017ad2a8b6f2fef91adb2a9afaed2cc
aakashmangla2000 <akkibaby90@yahoo.com>
2022-01-22 21:49:15 +0530

    submission added

---
+++ task4/task4C/FW_1066_Task_4C/task_4c_phoenix_server/lib/task_4c_phoenix_server_web/channels/robot_channel.ex
@@ -0,0 +1,188 @@
+defmodule Task4CPhoenixServerWeb.RobotChannel do
+  use Phoenix.Channel
+
+  @doc """
+  Handler function for any Client joining the channel with topic "robot:status".
+  Subscribe to the topic named "robot:update" on the Phoenix Server using Endpoint.
+  Reply or Acknowledge with socket PID received from the Client.
+  """
+  def join("robot:status", _params, socket) do
+    Task4CPhoenixServerWeb.Endpoint.subscribe("robot:update")
+    {:ok, socket}
+  end
+
+  @doc """
+  Callback function for messages that are pushed to the channel with "robot:status" topic with an event named "new_msg".
+  Receive the message from the Client, parse it to create another Map strictly of this format:
+  %{"client" => < "robot_A" or "robot_B" >,  "left" => < left_value >, "bottom" => < bottom_value >, "face" => < face_value > }
+
+  These values should be pixel locations for the robot's image to be displayed on the Dashboard
+  corresponding to the various actions of the robot as recevied from the Client.
+
+  Broadcast the created Map of pixel locations, so that the ArenaLive module can update
+  the robot's image and location on the Dashboard as soon as it receives the new data.
+
+  Based on the message from the Client, determine the obstacle's presence in front of the robot
+  and return the boolean value in this format {:ok, < true OR false >}.
+
+  If an obstacle is present ahead of the robot, then broadcast the pixel location of the obstacle to be displayed on the Dashboard.
+  """
+  def handle_in("new_msg", message, socket) do
+
+    # determine the obstacle's presence in front of the robot and return the boolean value
+    is_obs_ahead = Task4CPhoenixServerWeb.FindObstaclePresence.is_obstacle_ahead?(message["x"], message["y"], message["face"])
+
+    # file object to write each action taken by each Robot (A as well as B)
+    {:ok, out_file} = File.open("task_4c_output.txt", [:append])
+    # write the robot actions to a text file
+    IO.binwrite(out_file, "#{message["client"]} => #{message["x"]}, #{message["y"]}, #{message["face"]}\n")
+
+    ###########################
+    ## complete this funcion ##
+    ###########################
+    mp = %{"a" => 0, "b" => 1, "c" => 2, "d" => 3, "e" => 4, "f" => 5, "g" => 6}
+
+    msg2 = if(is_obs_ahead == false) do
+      %{"client" => message["client"], "left" => (message["x"]-1)*150, "bottom" => Map.get(mp, message["y"])*150, "face" => message["face"],  "obs" => is_obs_ahead, "x" => 0, "y" => 0}
+    else
+      facing = message["face"]
+      {x,y} = cond do
+        facing == "north" ->
+          {(message["x"]-1)*150,(Map.get(mp, message["y"])*150)+75}
+        facing == "south" ->
+          {(message["x"]-1)*150,(Map.get(mp, message["y"])*150)-75}
+        facing == "east" ->
+          {((message["x"]-1)*150)+75,(Map.get(mp, message["y"])*150)}
+        facing == "west" ->
+          {((message["x"]-1)*150)-75,(Map.get(mp, message["y"])*150)}
+      end
+      %{"client" => message["client"], "left" => (message["x"]-1)*150, "bottom" => Map.get(mp, message["y"])*150, "face" => message["face"], "obs" => is_obs_ahead, "x" => x, "y" => y}
+    end
+
+    if(msg2 !=  nil ) do
+      Phoenix.PubSub.broadcast(Task4CPhoenixServer.PubSub, "robot:update", msg2)
+    else
+    end
+
+    {:reply, {:ok, is_obs_ahead}, socket}
+  end
+
+  def handle_in("get_bots",message,socket) do
+    resource_id = {User, {:id, 1}}
+    update_user = fn(parent,message) ->
+      lock = Mutex.await(MyMutex, resource_id)
+      {ax,ay,afacing,bx,by,bfacing,a_start,b_start,a_alive,b_alive} = GenServer.call(Positions, :pop)
+      robots = if(message["client"] == "robot_A") do
+        %{ax: message["x"], ay: message["y"], afacing: message["face"], bx: bx, by: by, bfacing: bfacing, a_alive: message["alive"], b_alive: b_alive}
+      else
+        %{ax: ax, ay: ay, afacing: afacing, bx: message["x"], by: message["y"], bfacing: message["face"], a_alive: a_alive, b_alive: message["alive"]}
+      end
+      %{ax: ax, ay: ay, afacing: afacing, bx: bx, by: by, bfacing: bfacing ,a_alive: a_alive, b_alive: b_alive} = robots
+      GenServer.cast(Positions,{:push, {ax,ay,afacing,bx,by,bfacing,a_start,b_start,a_alive,b_alive}})
+      Mutex.release(MyMutex, lock)
+      send(parent,{:pos,[ax,ay,afacing,bx,by,bfacing,a_alive,b_alive]})
+    end
+    parent = self()
+    spawn(fn -> update_user.(parent,message) end)
+    [ax,ay,afacing,bx,by,bfacing,a_alive,b_alive] = receive do
+      {:pos, value} -> value
+    end
+
+    if a_alive == false and b_alive == false do
+      Task4CPhoenixServerWeb.Endpoint.broadcast("timer:stop", "stop_timer", %{})
+    end
+
+    rep = [ax,ay,afacing,bx,by,bfacing,a_alive,b_alive]
+    {:reply, {:ok, rep}, socket}
+  end
+
+  def handle_in("goals", message, socket) do
+    y = File.stream!("Plant_Positions.csv")
+    |> CSV.decode
+    |> Enum.map(fn {_,x} ->
+      x
+    end)
+
+    {_,y} = List.pop_at(y,0)
+    sow = Enum.map(y,fn [a,_b] ->
+      a
+    end)
+    weed = Enum.map(y,fn [_a,b] ->
+      b
+    end)
+    goals = if(message["client"] == "robot_A") do
+      sow
+    else
+      weed
+    end
+    Phoenix.PubSub.broadcast(Task4CPhoenixServer.PubSub, "robot:update", %{sow: sow, weed: weed})
+    {:reply, {:ok, goals}, socket}
+  end
+
+  def handle_in("start_pos", message, socket) do
+
+    if(GenServer.whereis(Positions) == nil) do
+      {:ok, _} = GenServer.start_link(Task4CPhoenixServer.Stack, [{"0","0","0","0","0","0",0,0,false,false}], name: Positions)
+    end
+
+    resource_id = {User, {:id, 1}}
+    update_user = fn(parent,message) ->
+      lock = Mutex.await(MyMutex, resource_id)
+      {ax,ay,afacing,bx,by,bfacing,a_start,b_start,a_alive,b_alive} = GenServer.call(Positions, :pop)
+      socket = if(message["client"] == "robot_A") do
+        assign(socket, :robotA_start, a_start)
+      else
+        assign(socket, :robotB_start, b_start)
+      end
+      GenServer.cast(Positions,{:push, {ax,ay,afacing,bx,by,bfacing,a_start,b_start,a_alive,b_alive}})
+      Mutex.release(MyMutex, lock)
+      send(parent,{:socket,socket})
+    end
+    parent = self()
+    spawn(fn -> update_user.(parent,message) end)
+    socket = receive do
+      {:socket, socket} -> socket
+    end
+
+    x = if(message["client"] == "robot_A") do
+      if(socket.assigns.robotA_start == 0) do
+        0
+      else
+        x = socket.assigns.robotA_start
+        [x]
+      end
+    else
+      if(socket.assigns.robotB_start == 0) do
+        0
+      else
+        x = socket.assigns.robotB_start
+        [x]
+      end
+    end
+
+    {:reply, {:ok, x}, socket}
+  end
+
+  #########################################
+  ## define callback functions as needed ##
+  #########################################
+
+  def handle_info(%{robotA_start: a, robotB_start: b} = _data, socket) do
+
+    resource_id = {User, {:id, 1}}
+    update_user = fn(a,b) ->
+      lock = Mutex.await(MyMutex, resource_id)
+      {ax,ay,afacing,bx,by,bfacing,_a_start,_b_start,a_alive,b_alive} = GenServer.call(Positions, :pop)
+      GenServer.cast(Positions,{:push, {ax,ay,afacing,bx,by,bfacing,a,b,a_alive,b_alive}})
+      Mutex.release(MyMutex, lock)
+    end
+    spawn(fn -> update_user.(a,b) end)
+    {:noreply, socket, 1000}
+  end
+
+  def handle_info(_data, socket) do
+
+    {:noreply, socket}
+  end
+
+end
---
+++ task4/task4C/FW_1066_Task_4C/task_4c_phoenix_server/lib/task_4c_phoenix_server_web/live/arena_live.ex
@@ -0,0 +1,296 @@
+defmodule Task4CPhoenixServerWeb.ArenaLive do
+  use Task4CPhoenixServerWeb,:live_view
+  require Logger
+
+  @doc """
+  Mount the Dashboard when this module is called with request
+  for the Arena view from the client like browser.
+  Subscribe to the "robot:update" topic using Endpoint.
+  Subscribe to the "timer:update" topic as PubSub.
+  Assign default values to the variables which will be updated
+  when new data arrives from the RobotChannel module.
+  """
+  def mount(_params, _session, socket) do
+
+    Task4CPhoenixServerWeb.Endpoint.subscribe("robot:update")
+    :ok = Phoenix.PubSub.subscribe(Task4CPhoenixServer.PubSub, "timer:update")
+
+    socket = assign(socket, :img_robotA, "robot_facing_north_a.png")
+    socket = assign(socket, :bottom_robotA, 0)
+    socket = assign(socket, :left_robotA, 0)
+    socket = assign(socket, :robotA_start, "")
+    socket = assign(socket, :robotA_goals, [])
+
+    socket = assign(socket, :img_robotB, "robot_facing_south_b.png")
+    socket = assign(socket, :bottom_robotB, 750)
+    socket = assign(socket, :left_robotB, 750)
+    socket = assign(socket, :robotB_start, "")
+    socket = assign(socket, :robotB_goals, [])
+
+    socket = assign(socket, :obstacle_pos, MapSet.new())
+    socket = assign(socket, :timer_tick, 300)
+
+    {:ok,socket}
+
+  end
+
+  @doc """
+  Render the Grid with the coordinates and robot's location based
+  on the "img_robotA" or "img_robotB" variable assigned in the mount/3 function.
+  This function will be dynamically called when there is a change
+  in the values of any of these variables =>
+  "img_robotA", "bottom_robotA", "left_robotA", "robotA_start", "robotA_goals",
+  "img_robotB", "bottom_robotB", "left_robotB", "robotB_start", "robotB_goals",
+  "obstacle_pos", "timer_tick"
+  """
+  def render(assigns) do
+
+    ~H"""
+    <div id="dashboard-container">
+
+      <div class="grid-container">
+        <div id="alphabets">
+          <div> A </div>
+          <div> B </div>
+          <div> C </div>
+          <div> D </div>
+          <div> E </div>
+          <div> F </div>
+        </div>
+
+        <div class="board-container">
+          <div class="game-board">
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+            <div class="box"></div>
+          </div>
+
+          <%= for obs <- @obstacle_pos do %>
+            <img  class="obstacles"  src="/images/stone.png" width="50px" style={"bottom: #{elem(obs,1)}px; left: #{elem(obs,0)}px"}>
+          <% end %>
+
+          <div class="robot-container" style={"bottom: #{@bottom_robotA}px; left: #{@left_robotA}px"}>
+            <img id="robotA" src={"/images/#{@img_robotA}"} style="height:70px;">
+          </div>
+
+          <div class="robot-container" style={"bottom: #{@bottom_robotB}px; left: #{@left_robotB}px"}>
+            <img id="robotB" src={"/images/#{@img_robotB}"} style="height:70px;">
+          </div>
+
+        </div>
+
+        <div id="numbers">
+          <div> 1 </div>
+          <div> 2 </div>
+          <div> 3 </div>
+          <div> 4 </div>
+          <div> 5 </div>
+          <div> 6 </div>
+        </div>
+
+      </div>
+      <div id="right-container">
+
+        <div class="timer-card">
+          <label style="text-transform:uppercase;width:100%;font-weight:bold;text-align:center" >Timer</label>
+            <p id="timer" ><%= @timer_tick %></p>
+        </div>
+
+        <div class="goal-card">
+          <div style="text-transform:uppercase;width:100%;font-weight:bold;text-align:center" > Goal positions </div>
+          <div style="display:flex;flex-flow:wrap;width:100%">
+            <div style="width:50%">
+              <label>Robot A</label>
+              <%= for i <- @robotA_goals do %>
+                <div><%= i %></div>
+              <% end %>
+            </div>
+            <div  style="width:50%">
+              <label>Robot B</label>
+              <%= for i <- @robotB_goals do %>
+              <div><%= i %></div>
+              <% end %>
+            </div>
+          </div>
+        </div>
+
+        <div class="position-card">
+          <div style="text-transform:uppercase;width:100%;font-weight:bold;text-align:center"> Start Positions </div>
+          <form phx-submit="start_clock" style="width:100%;display:flex;flex-flow:row wrap;">
+            <div style="width:100%;padding:10px">
+              <label>Robot A</label>
+              <input name="robotA_start" style="background-color:white;" value={"#{@robotA_start}"}>
+            </div>
+            <div style="width:100%; padding:10px">
+              <label>Robot B</label>
+              <input name="robotB_start" style="background-color:white;" value={"#{@robotB_start}"}>
+            </div>
+
+            <button  id="start-btn" type="submit">
+              <svg xmlns="http://www.w3.org/2000/svg" style="height:30px;width:30px;margin:auto" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z" clip-rule="evenodd" />
+              </svg>
+            </button>
+
+            <button phx-click="stop_clock" id="stop-btn" type="button">
+              <svg xmlns="http://www.w3.org/2000/svg" style="height:30px;width:30px;margin:auto" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zM8 7a1 1 0 00-1 1v4a1 1 0 001 1h4a1 1 0 001-1V8a1 1 0 00-1-1H8z" clip-rule="evenodd" />
+              </svg>
+            </button>
+          </form>
+        </div>
+
+      </div>
+
+    </div>
+    """
+
+  end
+
+  @doc """
+  Handle the event "start_clock" triggered by clicking
+  the PLAY button on the dashboard.
+  """
+  def handle_event("start_clock", data, socket) do
+    socket = assign(socket, :robotA_start, data["robotA_start"])
+    socket = assign(socket, :robotB_start, data["robotB_start"])
+    Task4CPhoenixServerWeb.Endpoint.broadcast("timer:start", "start_timer", %{})
+    #################################
+    ## edit the function if needed ##
+    #################################
+    Phoenix.PubSub.broadcast!(Task4CPhoenixServer.PubSub, "robot:update", %{robotA_start: data["robotA_start"],robotB_start: data["robotB_start"]})
+
+    {:noreply, socket}
+
+  end
+
+  @doc """
+  Handle the event "stop_clock" triggered by clicking
+  the STOP button on the dashboard.
+  """
+  def handle_event("stop_clock", _data, socket) do
+
+    Task4CPhoenixServerWeb.Endpoint.broadcast("timer:stop", "stop_timer", %{})
+
+    #################################
+    ## edit the function if needed ##
+    #################################
+
+    {:noreply, socket}
+
+  end
+
+  @doc """
+  Callback function to handle incoming data from the Timer module
+  broadcasted on the "timer:update" topic.
+  Assign the value to variable "timer_tick" for each countdown.
+  """
+  def handle_info(%{event: "update_timer_tick", payload: timer_data, topic: "timer:update"}, socket) do
+
+    Logger.info("Timer tick: #{timer_data.time}")
+    socket = assign(socket, :timer_tick, timer_data.time)
+
+    {:noreply, socket}
+
+  end
+
+  def handle_info(%{sow: sow, weed: weed} = _data, socket) do
+    socket = if(sow != nil) do
+        assign(socket, :robotA_goals,sow)
+      else
+        socket
+      end
+    socket = if(weed != nil) do
+        assign(socket, :robotB_goals,weed)
+      else
+        socket
+      end
+    {:noreply, socket}
+  end
+
+  @doc """
+  Callback function to handle any incoming data from the RobotChannel module
+  broadcasted on the "robot:update" topic.
+  Assign the values to the variables => "img_robotA", "bottom_robotA", "left_robotA",
+  "img_robotB", "bottom_robotB", "left_robotB" and "obstacle_pos" as received.
+  Make sure to add a tuple of format: { < obstacle_x >, < obstacle_y > } to the MapSet object "obstacle_pos".
+  These values msut be in pixels. You may handle these variables in separate callback functions as well.
+  """
+  def handle_info(data, socket) do
+
+    ###########################
+    ## complete this funcion ##
+    ###########################
+
+    socket = if(data["obs"] == true) do
+      assign(socket, :obstacle_pos,MapSet.put(socket.assigns.obstacle_pos,{data["x"],data["y"]}))
+    else
+      assign(socket, :obstacle_pos,socket.assigns.obstacle_pos)
+    end
+    socket = if(data["client"] == "robot_A") do
+      facing = data["face"]
+      socket = cond do
+        facing == "north" ->
+          assign(socket, :img_robotA, "robot_facing_north_a.png")
+        facing == "south" ->
+          assign(socket, :img_robotA, "robot_facing_south_a.png")
+        facing == "east" ->
+          assign(socket, :img_robotA, "robot_facing_east_a.png")
+        facing == "west" ->
+          assign(socket, :img_robotA, "robot_facing_west_a.png")
+        true ->
+          assign(socket, :img_robotA, "robot_facing_west_a.png")
+      end
+      socket = assign(socket, :bottom_robotA,data["bottom"])
+      assign(socket, :left_robotA,data["left"])
+
+    else
+      facing = data["face"]
+      socket = cond do
+        facing == "north" ->
+          assign(socket, :img_robotB, "robot_facing_north_b.png")
+        facing == "south" ->
+          assign(socket, :img_robotB, "robot_facing_south_b.png")
+        facing == "east" ->
+          assign(socket, :img_robotB, "robot_facing_east_b.png")
+        facing == "west" ->
+          assign(socket, :img_robotB, "robot_facing_west_b.png")
+        true ->
+          assign(socket, :img_robotB, "robot_facing_west_b.png")
+      end
+      socket = assign(socket, :bottom_robotB,data["bottom"])
+      assign(socket, :left_robotB,data["left"])
+    end
+
+    {:noreply, socket}
+
+  end
+
+  ######################################################
+  ## You may create extra helper functions as needed  ##
+  ## and update remaining assign variables.           ##
+  ######################################################
+
+end
